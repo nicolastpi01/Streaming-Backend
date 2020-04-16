19d2cef7391d7e2bc9620b6528da59f86a762b80
nicolastpi01 <nicolastpi10@gmail.com>
2020-04-15 21:24:29 -0300

    add async service traer video mp4

--- Streaming/Controllers/VideoController.cs
+++ Streaming/Controllers/VideoController.cs
@@ -8,6 +8,7 @@ using System.IO;
 using System.Net;
 using System.Collections.Generic;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.FileProviders;
 
 namespace Streaming.Controllers
 {
@@ -15,9 +16,8 @@ namespace Streaming.Controllers
     [ApiController]
     public class VideoController : ControllerBase
     {
-
-        private const string videoFilePath = "~/Movies/";
         private System.Collections.ArrayList listaVideos;
+        public static string downloadFilePath = @"C:\Users\NICO\source\repos\Streaming\Streaming\small.mp4";
 
         public VideoController(IConfiguration Configuration)
         {
@@ -32,88 +32,19 @@ namespace Streaming.Controllers
         {
             fileId = fileId < 0 ? 0 : fileId;
             fileId = fileId > 2 ? 2 : fileId;
-            return PhysicalFile(Path.GetFullPath(@listaVideos[fileId] as string), "application/octet-stream", enableRangeProcessing: true);
+            //string valor = @listaVideos[fileId] as string;
+            string path = Path.GetFullPath("small.mp4");
+
+            //IFileProvider provider = new PhysicalFileProvider(BASE_PATH);
+            //var fileInfo = provider.GetFileInfo(path);
+            //var fileStream = fileInfo.CreateReadStream();
+
+            var file2 = System.IO.File.Open(downloadFilePath, FileMode.Open);
+            
+            //return PhysicalFile(path, "application/octet-stream", enableRangeProcessing: true);
+            return File(file2, "application/octet-stream");
         }
 
-        /// Gets the live video.
-        [HttpGet]
-        //[Route("api/video/")] -> Si no tiene nada esta es la ruta por defecto
-        //public S.IHttpActionResult GetLiveVideo(string fileName)
-        public String GetLiveVideo()
-        {
-            //string filePath = Path.Combine(Path.GetDirectoryName(videoFilePath), "small.mp4");
-            //Console.WriteLine(filePath);
-            //return new VideoFileActionResult(filePath);
-            return "This is my default action...";
-        }
-
-        /// Gets the live video using post.
-        /// The request.
-        /*
-        [HttpPost]
-        [Route("api/video/live")]
-        public S.IHttpActionResult GetLiveVideoPost(VideoFileDownloadRequest request)
-        {
-            string filePath = Path.Combine(HttpContext.Current.Server.MapPath(videoFilePath), request.FileName);
-            return new VideoFileActionResult(filePath);
-        } 
-        */
     }
 
-    /// Action Result for Returning Stream
-    public class VideoFileActionResult : S.IHttpActionResult
-    {
-        private const long BufferLength = 65536;
-        public VideoFileActionResult(string videoFilePath)
-        {
-            this.Filepath = videoFilePath;
-        }
-
-        public string Filepath { get; private set; }
-
-        public Task<HttpResponseMessage> ExecuteAsync(CancellationToken cancellationToken)
-        {
-            HttpResponseMessage response = new HttpResponseMessage();
-            FileInfo fileInfo = new FileInfo(this.Filepath);
-            long totalLength = fileInfo.Length;
-            response.Content = new PushStreamContent((outputStream, httpContent, transportContext) =>
-            {
-                OnStreamConnected(outputStream, httpContent, transportContext);
-            });
-
-            response.Content.Headers.ContentLength = totalLength;
-            return Task.FromResult(response);
-        }
-
-        private async void OnStreamConnected(Stream outputStream, HttpContent content, TransportContext context)
-        {
-            try
-            {
-                var buffer = new byte[BufferLength];
-
-                using (var nypdVideo = File.Open(this.Filepath, FileMode.Open, FileAccess.Read, FileShare.Read))
-                {
-                    var videoLength = (int)nypdVideo.Length;
-                    var videoBytesRead = 1;
-
-                    while (videoLength > 0 && videoBytesRead > 0)
-                    {
-                        videoBytesRead = nypdVideo.Read(buffer, 0, Math.Min(videoLength, buffer.Length));
-                        await outputStream.WriteAsync(buffer, 0, videoBytesRead);
-                        videoLength -= videoBytesRead;
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                return;
-            }
-            finally
-            {
-                // Close output stream as we are done
-                outputStream.Close();
-            }
-        }
-        
-    }
 }
--- Streaming/Startup.cs
+++ Streaming/Startup.cs
@@ -11,8 +11,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Streaming.Hubs;
-using Streaming.Services;
 
 namespace Streaming
 {
