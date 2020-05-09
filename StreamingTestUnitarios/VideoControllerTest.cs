51527f6dbc6344a2f9557ff6ba1d3eb08779ea77
OttoOctavius <OttoOctavius@users.noreply.github.com>
2020-05-09 01:27:59 -0300

    Paquetes y test

--- StreamingTestUnitarios/VideoControllerTest.cs
+++ StreamingTestUnitarios/VideoControllerTest.cs
@@ -1,18 +1,47 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using AutoFixture;
+using AutoFixture.AutoMoq;
+using AutoFixture.Xunit2;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 using Streaming.Controllers;
 using Streaming.Infraestructura;
+using Streaming.Infraestructura.Entities;
 using Xunit;
 
 namespace StreamingTestUnitarios
 {
     public class VideoControllerTest
     {
-        [Fact]
-        public void Test1()
+        public DbSet<MediaEntity> BuildDbSet(IQueryable<MediaEntity> data)
         {
-            var mediaC = new Mock<MediaContext>();
-            var controlador = new VideoController(mediaC.Object);
+            var dbSet = new Mock<DbSet<MediaEntity>>();
+            dbSet.As<IQueryable<MediaEntity>>().Setup(m => m.Provider).Returns(data.Provider);
+            dbSet.As<IQueryable<MediaEntity>>().Setup(m => m.Expression).Returns(data.Expression);
+            dbSet.As<IQueryable<MediaEntity>>().Setup(m => m.ElementType).Returns(data.ElementType);
+            dbSet.As<IQueryable<MediaEntity>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+            return dbSet.Object;
         }
+
+        [Fact]
+        public void Get_ShouldReturn_HttpStatusOk()
+        {
+            var fixture = new Fixture()
+                .Customize(new AutoMoqCustomization());
+            var sutMediaC = fixture.Freeze<MediaContext>();
+
+            var data = new List<MediaEntity>
+            {
+                new MediaEntity { }
+            }.AsQueryable();
+
+            sutMediaC.Medias = BuildDbSet(data);
+            var controlador = new VideoController(sutMediaC);
+
+            controlador.GetVideos(1).Wait();
+        }
+
     }
 }
